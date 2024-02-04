938d0277695068750993a0cb2bbd63595b720cc8
Ravi Singh <ravi149185@gmail.com>
2024-02-04 15:49:02 +0530

    Add configuration files and update app module

---
+++ config/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+export const applicationConfig = {
+  app: {
+    env: process.env.NODE_ENV,
+    port: process.env.NODE_ENV,
+  },
+
+  // Database
+  db: {
+    dbDialect: process.env.DB_DIALECT || 'postgres',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as morgan from 'morgan';
+import * as cookieParser from 'cookie-parser';
+
+import { applicationConfig } from 'config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(morgan('combined'));
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: [`'self'`, 'unpkg.com'],
+          styleSrc: [
+            `'self'`,
+            `'unsafe-inline'`,
+            'cdn.jsdelivr.net',
+            'fonts.googleapis.com',
+            'unpkg.com',
+          ],
+          fontSrc: [`'self'`, 'fonts.gstatic.com', 'data:'],
+          imgSrc: [`'self'`, 'data:', 'cdn.jsdelivr.net'],
+          scriptSrc: [
+            `'self'`,
+            `https: 'unsafe-inline'`,
+            `cdn.jsdelivr.net`,
+            `'unsafe-eval'`,
+          ],
+        },
+      },
+    }),
+  );
+
+  await app.listen(applicationConfig.app.port, '0.0.0.0');
+
+  console.log(`Application is running on: ${applicationConfig.app.port}`);
 }
 bootstrap();
